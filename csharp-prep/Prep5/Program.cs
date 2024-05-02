e018e4c7547bc02800b977664296b9e10ef3fdd0
iCarrin isaiahcarrin@gmail.com <isaiahcarring@gmail.com>
2024-05-01 18:14:18 -0600

    made functions program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userSquare = SquareNumber(userNumber);
+        DisplayResult(userName, userSquare);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number");
+        int favNumb = int.Parse(Console.ReadLine());
+        return favNumb;
+
+    }
+    static int SquareNumber(int num)
+    {
+       int sqNumb = num * num;
+       return sqNumb;
+    }
+    static void DisplayResult(string name, int num)
+    {
+        Console.WriteLine($"{name} your favorite number squared in {num}");
     }
 }
